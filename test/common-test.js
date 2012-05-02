62aae08fee668ba645e44a6096735693566200e0
Richard Rodger <richard@ricebridge.com>
2012-05-02 14:37:34 +0100

    deepoverride

--- test/common-test.js
+++ test/common-test.js
@@ -8,12 +8,12 @@ var common = require('../lib/common.js')
 
 
 vows.describe('common').addBatch({
-  'interpolate': {
+  'basic': {
     topic: function() {
       return common;
     },
 
-    'basic': function( common ) {
+    'interpolate': function( common ) {
       assert.equal( common.interpolate('',{}), '' )
       assert.equal( common.interpolate('-#{p1}-',{p1:'v1'}), '-v1-' )
       assert.equal( common.interpolate('#{p1}-#{p2}-#{p1}',{p1:'v1',p2:'v2'}), 'v1-v2-v1' )
@@ -23,6 +23,19 @@ vows.describe('common').addBatch({
 
       // TODO: test error handling
     },
+
+    'deepoverride': function( common ) {
+      assert.equal( JSON.stringify( common.deepoverride({a:1,b:2},{b:22,c:3})), 
+                    '{"a":1,"b":22,"c":3}' )
+      assert.equal( JSON.stringify( common.deepoverride({a:1,b:2,c:{c:1,d:1}},{b:22,c:{d:2,e:3},d:3})), 
+                    '{"a":1,"b":22,"c":{"c":1,"d":2,"e":3},"d":3}' )
+      assert.equal( JSON.stringify( common.deepoverride({a:1,b:[1]},{b:[1,2],c:3})), 
+                    '{"a":1,"b":[1,2],"c":3}' )
+      assert.equal( JSON.stringify( common.deepoverride({a:1,b:[1,2]},{b:[3],c:3})), 
+                    '{"a":1,"b":[3,2],"c":3}' )
+      assert.equal( JSON.stringify( common.deepoverride({a:1,b:[1,2]},{b:{1:3},c:3})), 
+                    '{"a":1,"b":[1,3],"c":3}' )
+    }
   },
 }).export(module)
 
