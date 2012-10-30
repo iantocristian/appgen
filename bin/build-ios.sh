6a1f15a454df36749cabbf70a53b97d566715156
iantocristian <iantocristian@yahoo.com>
2012-10-30 04:25:05 +0200

    ios build: copy provisioning profile to user library folder before running xcodebuild

--- bin/build-ios.sh
+++ bin/build-ios.sh
@@ -15,12 +15,17 @@ keychain_password="$6"
 code_sign_identity="$7"
 provisioning_profile="$8"
 
-security unlock-keychain -p "$keychain_password" "$keychain"
-security default-keychain -s "$keychain"
-
 if [ ! -d "$output_dir" ]; then
   mkdir "$output_dir"
 fi
+
+LOG="$output_dir/appgen.log"
+
+echo "security unlock-keychain -p \"$keychain_password\" \"$keychain\"" >> $LOG
+security unlock-keychain -p "$keychain_password" "$keychain" >> $LOG
+
+echo "security default-keychain -s \"$keychain\"" >> $LOG
+security default-keychain -s "$keychain" >> $LOG
 
 current_dir=`pwd`
 working_dir=${current_dir}
@@ -32,10 +37,27 @@ output_dir=${current_dir}
 cd "$working_dir"
 
 if [ -f "$output_dir/$target_name.app" ]; then
-  rm "$output_dir/$target_name.app"
+  rm "$output_dir/$target_name.app" >> $LOG
 fi
 
-xcodebuild -verbose -project "$project_file" -target "$target_name" -sdk iphoneos -configuration $configuration clean build CONFIGURATION_BUILD_DIR="$output_dir"
+echo "\"$provisioning_profile\" \"$HOME/Library/MobileDevice/Provisioning Profiles/temporary.mobileprovision\"" >> $LOG
+cp "$provisioning_profile" "$HOME/Library/MobileDevice/Provisioning Profiles/temporary.mobileprovision" >> $LOG
+
+echo "xcodebuild -verbose -project \"$project_file\" -target \"$target_name\" -sdk iphoneos \
+  -configuration $configuration clean build \
+  CONFIGURATION_BUILD_DIR=\"$output_dir\" \
+" >> $LOG
+
+xcodebuild -verbose -project "$project_file" -target "$target_name" -sdk iphoneos \
+  -configuration $configuration clean build \
+  CONFIGURATION_BUILD_DIR="$output_dir" \
+  # PROVISIONING_PROFILE="temporary" \
+  # CODE_SIGN_IDENTITY="$code_sign_identity" \
+  # OTHER_CODE_SIGN_FLAGS="--keychain $keychain" \
+ >> $LOG
+
+echo "rm \"$HOME/Library/MobileDevice/Provisioning Profiles/temporary.mobileprovision\"" >> $LOG
+rm "$HOME/Library/MobileDevice/Provisioning Profiles/temporary.mobileprovision" >> $LOG
 
 if [ $? -ne 0 ]; then
   echo "Build failed."
